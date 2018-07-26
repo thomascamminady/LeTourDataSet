3a30460e4f10771a6ad8694b66b08ca2b097655a
Thomas <camminady@mathcces.rwth-aachen.de>
2018-07-26 06:49:33 +0200

    Mentioning errors

--- README.txt
+++ README.txt
@@ -1,1 +1,13 @@
 See http://www.camminady.org/every-cyclist-of-the-tour-de-france-in-a-single-csv-file/ for a description.
+
+The following problems are known:
+1905 - 0
+1906 - 0    
+1907 - p
+1908 - 0
+1909 - p
+1910 - p
+1911 - p
+1912 - p
+Where 0 means that all times are zero and p means that the column for the timing actually contained the overall points and not the timings (this was an error on the LeTour.fr website).
+
--- main.R
+++ main.R
@@ -22,3 +22,24 @@ summary <- read_csv("summary.csv",
                       avgpace = col_double()
                     ))
 
+
+ggplot(data=summary)+
+  geom_point(aes(x=year,y=km,colour="Distance"))+
+  geom_point(aes(x=year,y=avgpace*100,colour="Pace"))+
+  scale_y_continuous(sec.axis = sec_axis(~.*0.01, name = "Avg. Pace in km/h"))+
+  scale_colour_manual(values = c("blue", "red"))+
+  labs(y = "Distance in km",
+              x = "Year",
+              colour = "Parameter")+
+  theme(legend.position = c(0.8, 0.9))+
+  ggtitle("Le Tour de France")+
+  theme_minimal()
+
+
+
+df$overalltime <- df$h*3600+df$m*60+df$s
+ggplot(data=df,aes(group=year,y=df$overalltime))+
+  geom_boxplot()
+
+ggplot(data=df)+geom_point(aes(x=year,y=overalltime))
+
