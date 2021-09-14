5d7e6f0412e406bcfe2befcf4015367d7307a49d
Thomas Camminady <thomas.camminady@wahoofitness.com>
2021-09-14 09:36:59 +0200

    Notebook to python script

---
+++ src/pipinstall.py
@@ -0,0 +1,9 @@
+# %%
+from IPython import get_ipython
+
+get_ipython().system("pip install requests")
+get_ipython().system("pip install html2text")
+get_ipython().system("pip install beautifulsoup4")
+get_ipython().system("pip install lxml")
+get_ipython().system("pip install html5lib")
+get_ipython().system("pip install pandas-read-xml")
